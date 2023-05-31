44295431313657c19887d83cfd5c57061309d352
Noor Zahra <114682660+NoorZahra12@users.noreply.github.com>
2023-05-31 22:21:59 +0400

    Add files via upload

---
+++ characters.js
@@ -0,0 +1,236 @@
+//These are the sounds of all the characters in the game
+
+//Anemo characters
+//venti, jean, sucrose, xiao, kazuha, sayu, heisou, wanderer, faruzan
+
+//first i name a value as the character name = new Audio()
+var venti = new Audio();
+//then I will add the audio from my folder/files in the variable which contains the audio
+venti.src = "venti.mp3"
+
+//duplicating it and changing the name of character and doing the same afterwards for all characters with their own audio
+var jean = new Audio();
+jean.src = "jean.mp3"
+
+var sucrose = new Audio();
+sucrose.src = "sucrose.mp3"
+
+var xiao = new Audio();
+xiao.src = "xiao.mp3"
+
+var kazuha = new Audio();
+kazuha.src = "kazuha.mp3"
+
+var sayu = new Audio();
+sayu.src = "sayu.mp3"
+
+var heizou = new Audio();
+heizou.src = "heizou.mp3"
+
+var wanderer = new Audio();
+wanderer.src = "wanderer.mp3"
+
+var faruzan = new Audio();
+faruzan.src = "faruzan.mp3"
+
+
+//Geo characters
+//zhongli, albedo, ningguang, itto, gorou, noelle, yunjin
+
+var zhongli = new Audio();
+zhongli.src = "zhongli.mp3"
+
+var albedo = new Audio();
+albedo.src = "albedo.mp3"
+
+var ningguang = new Audio();
+ningguang.src = "ningguang.mp3"
+
+var itto = new Audio();
+itto.src = "itto.mp3"
+
+var gorou = new Audio();
+gorou.src = "gorou.mp3"
+
+var noelle = new Audio();
+noelle.src = "noelle.mp3"
+
+var yunjin = new Audio();
+yunjin.src = "yunjin.mp3"
+
+//Electro characters
+//raiden, yae miko, beidou, fischl, lisa, razor, sara, dori, cyno, keqing, kuki
+
+var raiden = new Audio();
+raiden.src = "raiden.mp3"
+
+var yaemiko = new Audio();
+yaemiko.src = "yaemiko.mp3"
+
+var beidou = new Audio();
+beidou.src = "beidou.mp3"
+
+var fischl = new Audio();
+fischl.src = "fischl.mp3"
+
+var oz = new Audio();
+oz.src = "oz.mp3"
+
+var lisa = new Audio();
+lisa.src = "lisa.mp3"
+
+var razor = new Audio();
+razor.src = "razor.mp3"
+
+var sara = new Audio();
+sara.src = "sara.mp3"
+
+var dori = new Audio();
+dori.src = "dori.mp3"
+
+var cyno = new Audio();
+cyno.src = "cyno.mp3"
+
+var keqing = new Audio();
+keqing.src = "keqing.mp3"
+
+var kuki = new Audio();
+kuki.src = "kuki.mp3"
+
+//Dendro characters
+//nahida, alhaitham, collei, tighnari, yaoyao, kaveh, kirara, baizhu
+
+var nahida = new Audio();
+nahida.src = "nahida.mp3"
+
+var alhaitham = new Audio();
+alhaitham.src = "alhaitham.mp3"
+
+var collei = new Audio();
+collei.src = "collei.mp3"
+
+var kuki = new Audio();
+kuki.src = "kuki.mp3"
+
+var tighnari = new Audio();
+tighnari.src = "tighnari.mp3"
+
+var yaoyao = new Audio();
+yaoyao.src = "yaoyao.mp3"
+
+var kaveh = new Audio();
+kaveh.src = "kaveh.mp3"
+
+var kirara = new Audio();
+kirara.src = "kirara.mp3"
+
+var baizhu = new Audio();
+baizhu.src = "baizhu.mp3"
+
+//Hydro characters
+//Barbara, ayato, mona , tartaglia, xingqiu, yelan, kokomi, nilou, candance
+
+var barbara = new Audio();
+barbara.src = "barbara.mp3"
+
+var ayato = new Audio();
+ayato.src = "ayato.mp3"
+
+var mona = new Audio();
+mona.src = "mona.mp3"
+
+var tartaglia = new Audio();
+tartaglia.src = "tartaglia.mp3"
+
+var xingqiu = new Audio();
+xingqiu.src = "xingqiu.mp3"
+
+var yelan = new Audio();
+yelan.src = "yelan.mp3"
+
+var kokomi = new Audio();
+kokomi.src = "kokomi.mp3"
+
+var nilou = new Audio();
+nilou.src = "nilou.mp3"
+
+var candace = new Audio();
+candace.src = "candace.mp3"
+
+//Pyro characters
+//diluc, klee, ben, yoimiya, hutao, dehya, xianling, thoma, xinyan, yanfei, amber
+
+var diluc = new Audio();
+diluc.src = "diluc.mp3"
+
+var klee = new Audio();
+klee.src = "klee.mp3"
+
+var ben = new Audio();
+ben.src = "bennett.mp3"
+
+var yoimiya = new Audio();
+yoimiya.src = "yoimiya.mp3"
+
+var hutao = new Audio();
+hutao.src = "hu tao.mp3"
+
+var dehya = new Audio();
+dehya.src = "dehya.mp3"
+
+var xiangling = new Audio();
+xiangling.src = "xiangling.mp3"
+
+var thoma = new Audio();
+thoma.src = "thoma.mp3"
+
+var xinyan = new Audio();
+xinyan.src = "xinyan.mp3"
+
+var yanfei = new Audio();
+yanfei.src = "yanfei.mp3"
+
+var amber = new Audio();
+amber.src = "amber.mp3"
+
+//Cryo characters
+//ayaka, ganyu, mika, rosaria, kaeya, eula, layla, qiqi, shenhe, chongyun, diona, aloy
+
+var ayaka = new Audio();
+ayaka.src = "ayaka.mp3"
+
+var ganyu = new Audio();
+ganyu.src = "ganyu.mp3"
+
+var mika = new Audio();
+mika.src = "mika.mp3"
+
+var mona = new Audio();
+mona.src = "mona.mp3"
+
+var rosaria = new Audio();
+rosaria.src = "rosaria.mp3"
+
+var kaeya = new Audio();
+kaeya.src = "kaeya.mp3"
+
+var eula = new Audio();
+eula.src = "eula.mp3"
+
+var layla = new Audio();
+layla.src = "layla.mp3"
+
+var qiqi = new Audio();
+qiqi.src = "qiqi.mp3"
+
+var shenhe = new Audio();
+shenhe.src = "shenhe.mp3"
+
+var chongyun = new Audio();
+chongyun.src = "chongyun.mp3"
+
+var diona = new Audio();
+diona.src = "diona.mp3"
+
+var aloy = new Audio();
+aloy.src = "aloy.mp3"
---
+++ home.js
@@ -0,0 +1,14 @@
+var yoimiya = new Audio();
+yoimiya.src = "yoimiya.mp3"
+
+var yaemiko = new Audio();
+yaemiko.src = "yaemiko.mp3"
+
+var kirara = new Audio();
+kirara.src = "kirara.mp3"
+
+var kazuha = new Audio();
+kazuha.src = "kazuha.mp3"
+
+var alhaitham = new Audio();
+alhaitham.src = "alhaitham.mp3"
