d0132a341ba2007b6f892276297029abe9bdc094
Noor Zahra <114682660+NoorZahra12@users.noreply.github.com>
2023-05-31 22:39:58 +0400

    Add files via upload

---
+++ teyvat.js
@@ -0,0 +1,14 @@
+//first i name a value as the character name = new Audio()
+var venti = new Audio();
+//then I will add the audio from my folder/files in the variable which contains the audio
+venti.src = "venti.mp3"
+
+var zhongli = new Audio();
+zhongli.src = "zhongli.mp3"
+
+var raiden = new Audio();
+raiden.src = "raiden.mp3"
+
+var nahida = new Audio();
+nahida.src = "nahida.mp3"
+
